123661ec645ef7b5870cbee7ade21c61cde807e8
rsgm1978 <159719756+rsgm1978@users.noreply.github.com>
2024-09-13 19:11:36 +0000

    comentariosContext3

--- src/front/js/component/ContextExample.jsx
+++ src/front/js/component/ContextExample.jsx
@@ -1,17 +1,18 @@
-/*importar el hook useContext
+/*1 importar el hook useContext
 import React { useContext} from "react";
- importamos Context desde el archivo appContext.js
+ 2 importamos Context desde el archivo appContext.js
 import { Context } from "../store/appContext.js";
 
 export const ContextExample = () => {
-    utilizo: Desectructurando store y el actions de Context el useContext
+    3 utilizo: Desectructurando store y el actions de Context el useContext
     const {store, actions} = useContext(Context)
 
     return  (
         <div className="container texte-center border mt-3">
-        utilizo la sintaxis store.clave para poder mostrar el valor
+        4 utilizo la sintaxis store.clave para poder mostrar el valor
             <p>{'store.user'}</p>
             <p>{'store.cohorte'}</p>
         </div>
         )
-    }*/
+    } 
+        esto se importa en el home como un componente*/

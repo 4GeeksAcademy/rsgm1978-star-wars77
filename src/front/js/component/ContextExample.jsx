111a786da4bdfeb6481a11150a1e0ddadac33ca8
rsgm1978 <159719756+rsgm1978@users.noreply.github.com>
2024-09-13 18:43:18 +0000

    comentariosContext

---
+++ src/front/js/component/ContextExample.jsx
@@ -0,0 +1,17 @@
+/*importar el hook useContext
+import React { useContext} from "react";
+ importamos Context desde el archivo appContext.js
+import { Context } from "../store/appContext.js";
+
+export const ContextExample = () => {
+    utilizo: Desectructurando store y el actions de Context el useContext
+    const {store, actions} = useContext(Context)
+
+    return  (
+        <div className="container texte-center border mt-3">
+        utilizo
+            <p>{'store.user'}</p>
+            <p>{'store.cohorte'}</p>
+        </div>
+        )
+    }*/

35d8f98487ffa08f9eaf825e343e0ac78b487ef5
rsgm1978 <159719756+rsgm1978@users.noreply.github.com>
2024-09-25 19:42:32 +0000

    changes for up

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,62 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.string(80)), unique=False, nullable=True)
+    last_name = db.Column(db.string(80)), unique=False, nullable=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        # Do not serialize the password, its a security breach
+        return {"id": self.id,
+                "email": self.email,
+                'is_active': self.is_active}
+    
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=False, nullable=False)
+    description = db.Column(db.String, unique=False, nullable=False)
+    body = db.Column(db.String, unique=False, nullable=False)
+    date = db.Column(db.DateTime, nullable=False) # Valor por defecto
+    image_url = db.Column(db.String)  # url, La obtenemos de cloudinary
+
+
+class Coments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, unique=False, nullable=False)
+
+
+class Medias(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.Enum('image', 'video', 'podcast', name='media_type'))
+
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+
+class CharactersFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+
+class PlanetFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+# Model Table    
--- src/app.py
+++ src/app.py
@@ -11,7 +11,6 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
-# from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
@@ -26,39 +25,37 @@ if db_url is not None:
         "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-
+# Other configurations
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
-# add the admin
+
+# Add the admin
 setup_admin(app)
 
-# add the admin
+
+# Add the admin
 setup_commands(app)
 
 # Add all endpoints form the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
-
-
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 
-
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
 
-
+# Any other endpoint will try to serve it like a static file
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
@@ -68,7 +65,7 @@ def serve_any_other_file(path):
     return response
 
 
-# this only runs if `$ python src/main.py` is executed
+# This only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
